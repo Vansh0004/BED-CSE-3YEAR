2a8adf064ae6c69def77009c69dd588cd51e91f0
Vansh Vaashisth <vanshvaashisth2006@gmail.com>
2025-07-04 15:05:12 +0530

    Added Lecture2 folder

--- Lecture2/file1.js
+++ Lecture2/file1.js
@@ -1,9 +1,11 @@
+let file2=require("./file3");
+console.log("file1");
 function add(a, b){
     return a + b;
 }
 function sub(a, b){
     return a - b;
 }
-
+`
 module.exports.add = add;
-module.exports.sub = sub;
+module.exports.sub = sub;`
